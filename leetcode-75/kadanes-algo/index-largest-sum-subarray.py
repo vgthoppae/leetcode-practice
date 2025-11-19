27ba4255bd33c8a970e598cd10192c92cf2b8a0e
vgthoppae <vgthoppae@gmail.com>
2025-11-18 23:16:16 -0500

    kadanes algo

---
+++ leetcode-75/kadanes-algo/index-largest-sum-subarray.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution:
+  def indexLargestSum(self, nums: List[int]) ->tuple[int]:
+    currsum = 0
+    maxsum = float('inf')
+    L = 0
+    maxL, maxR = 0, 0
+
+    for R in range(len(nums)):
+      if currsum < 0:
+        currsum = 0
+        L = R
+      currsum += nums[R]
+      if currsum > maxsum:
+        maxsum = currsum
+        maxL = L
+        maxR = R
+
+    return (maxL, maxR)
+
+if __name__ == "__main__":
+  s= Solution()
+  nums = [4, -1, 2, -7, 3, 4]
+  print(s.indexLargestSum(nums))
